a3dcb49ba49d49130023bcec8707b4c8637f1ea9
Ryan Belisle <ryan@ryebell.me>
2015-12-05 22:02:19 -0500

    update to create class for checkCertInfo.php

--- src/checkCertInfo.php
+++ src/checkCertInfo.php
@@ -1,14 +1,28 @@
 <?php
-$g = stream_context_create (array("ssl" => array("capture_peer_cert" => true)));
-$r = stream_socket_client("ssl://$DOMAIN:$PORT", $errno, $errstr, 30,
-    STREAM_CLIENT_CONNECT, $g);
-$cont = stream_context_get_params($r);
-$cert = openssl_x509_read($cont["options"]["ssl"]["peer_certificate"]);
-$cert_data = openssl_x509_parse( $cert );
-$common_name = $cert_data['subject']['CN'];
-$issuer = $cert_data['issuer']['O'];
-$valid_from = $cert_data['validFrom_time_t']; 
-$valid_to = $cert_data['validTo_time_t'];
 
-echo "Common Name: " . $common_name . "\n" . "Issuing CA: " . $issuer . "\n" . "Valid from: " . $valid_from . "\n" . "Valid to: " . $valid_to . "\n";
+class CertificateScanning {
+    private $target;
+    private $target_port;
+
+    public function set_parameters($target, $target_port)
+    {
+        $this->target=$target;
+        $this->target_port=$target_port;
+    }
+
+    public function scan_certificate()
+    {
+        $g = stream_context_create (array("ssl" => array("capture_peer_cert" => true)));
+        $r = stream_socket_client("ssl://$this->target:$this->target_port", $errno, $errstr, 30,
+                STREAM_CLIENT_CONNECT, $g);
+        $cont = stream_context_get_params($r);
+        $cert = openssl_x509_read($cont["options"]["ssl"]["peer_certificate"]);
+        $cert_data = openssl_x509_parse( $cert );
+        $this->common_name=$cert_data['subject']['CN'];
+        $this->issuer=$cert_data['issuer']['O'];
+        $this->valid_from=$cert_data['validFrom_time_t'];
+        $this->valid_to=$cert_data['validTo_time_t'];
+    }
+}
+
 ?>
