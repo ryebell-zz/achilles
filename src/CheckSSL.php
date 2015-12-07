12e71f9dacbb88798bf9a3851a2bc856561e4785
Ryan Belisle <ryan@ryebell.me>
2015-12-06 20:09:55 -0500

    remove comment

--- src/CheckSSL.php
+++ src/CheckSSL.php
@@ -8,8 +8,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#require '../vendor/autoload.php';
-
 class CheckSSL extends Command {
 
     public function configure()
